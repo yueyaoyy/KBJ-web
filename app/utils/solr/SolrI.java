89627c403b6e61b8d3ee25de3045f4c782a3e7e8
jie-zhang <jie-zhang@rontech.co.jp>
2017-11-22 18:10:50 +0800

    Rename the solr util class name.

--- app/utils/solr/SolrI.java
+++ app/utils/solr/SolrI.java
@@ -7,25 +7,47 @@ import java.util.List;
 public interface SolrI {
 
     /**
-     * 根据name检索
+     *
+     * @param keyword
+     * @param start
+     * @param rows
+     * @param sort eg: sort=price desc,score asc
+     * @param fq  eg: fq=price:[100 To *]&fq=section:0
+     * @return
+     */
+    //List<Product> query(String keyword, int start, int rows, String sort, String fq);
+
+    /**
+     * find from solr by the product skuid.
+     * @param skuid
+     * @return
+     */
+    //Product query(String skuid);
+
+
+
+
+
+    /**
      * @param name
      * @param start 分页(从0开始)
      * @param sort 排序
      * @param order 1：desc，0：asc
      * @param fq 过滤器查询,例:("id:jd_000000", "price:[1 TO 99]")
      */
-    public List<Product> searchProductByName(String name, int start, String sort, int order, String... fq);
+    List<Product> searchProductByName(String name, int start, String sort, int order, String... fq);
+
 
     /**
      * 根据id检索
      * @param id
      * @return
      */
-    public Product searchProductById(String id);
+    Product searchProductById(String id);
 
-    /**
-     * 删除所有索引
-     */
-    public void deleteAll();
+//    /**
+//     * 删除所有索引
+//     */
+//    void deleteAll();
 
 }
--- app/utils/solr/SolrUtil.java
+++ app/utils/solr/SolrUtil.java
@@ -10,7 +10,24 @@ import org.apache.solr.client.solrj.response.QueryResponse;
 import java.io.IOException;
 import java.util.List;
 
-public class SolrHttpUtil implements SolrI {
+public class SolrUtil implements SolrI {
+
+
+//    defType	指定用于处理查询语句(参数q的内容)的查询解析器，eg:defType=lucene
+//    sort	指定响应的排序方式：升序asc或降序desc.同时需要指定按哪个字段进行排序。eg: sort=price desc,score asc
+//    start	指定显示查询结果的开始位置，默认是0
+//    rows	指定一次显示多少行查询结果，默认是10
+//    fq	指定用于对查询结果进行过滤的过滤器(也看作是一种query) eg: fq=price:[100 To *]&fq=setction:0
+//    fl	指定查询结果中返回的字段，该字段的stored=”true”或docValues=”true” ,eg:fl=id,title,product(price, popularity)
+//    debug	指定查询结果中携带额外的调试信息：时间信息debug=timing，“explain”信息debug=results,所有调试信息debug=query
+//    explainOther	Allows clients to specify a Lucene query to identify a set of documents. If non-blank,the explain info of each document which matches this query, relative to the main query (specified by the q parameter) will be returned along with the rest of the debugging information.
+//    timeAllowed	指定查询处理的时间，单位毫秒。如果查询在指定的时间未完成，则只返回部分信息
+//    segmentTerminateEarly	Indicates that, if possible, Solr should stop collecting documents from each individual (sorted) segment once it can determine that any subsequent documents in that segment will not be candidates for the rows being returned. The default is false.
+//    omitHeader	当设为true时，返回结果不包含头部信息(例如请求花费的时间等信息)，默认是false
+//    wt	执行响应的输出格式：xml或json等
+//    logParamsList	指定哪些参数需要写入log, eg:logParamsList=q,fq
+//    echoParams	指定响应头部包含哪些参数，取值为none/all/explicit(默认值)
+
 
     private static final String URL = "http://192.168.0.3:8983/solr";
 
@@ -63,24 +80,24 @@ public class SolrHttpUtil implements SolrI {
         return getProducts(PRODUCTS, query).get(0);
     }
 
-    /**
-     * 删除所有索引
-     */
-    public void deleteAll() {
-        SolrClient client = this.getHttpClient();
-
-        try {
-            //Deleting the documents from SolrI
-            client.deleteByQuery(PRODUCTS,"*");
-
-            //Saving the document
-            client.commit();
-        } catch(SolrServerException e) {
-            e.printStackTrace();
-        } catch(IOException e) {
-            e.printStackTrace();
-        }
-    }
+//    /**
+//     * 删除所有索引
+//     */
+//    public void deleteAll() {
+//        SolrClient client = this.getHttpClient();
+//
+//        try {
+//            //Deleting the documents from SolrI
+//            client.deleteByQuery(PRODUCTS,"*");
+//
+//            //Saving the document
+//            client.commit();
+//        } catch(SolrServerException e) {
+//            e.printStackTrace();
+//        } catch(IOException e) {
+//            e.printStackTrace();
+//        }
+//    }
 
     /**
      * 根据检索条件，取得商品List
